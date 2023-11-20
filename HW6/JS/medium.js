83ad89210676e3b5fffc0ddf7c38502531facfbd
Christ <Christ42@vtb.link>
2023-11-19 21:23:10 -0500

    hw6

---
+++ HW6/JS/medium.js
@@ -0,0 +1,53 @@
+function changeImage1() {
+  var image = document.getElementById("img1");
+  if (image.src.match("img1")) {
+    image.src = "./Assets/img2.png";
+  } else {
+    image.src = "./Assets/img1.png";
+  }
+}
+
+function changeImage3() {
+  var image = document.getElementById("img3");
+  if (image.src.match("img3")) {
+    image.src = "./Assets/img4.png";
+  } else {
+    image.src = "./Assets/img3.png";
+  }
+}
+
+function changeImage5() {
+  var image = document.getElementById("img5");
+  if (image.src.match("img5")) {
+    image.src = "./Assets/img6.png";
+  } else {
+    image.src = "./Assets/img5.png";
+  }
+}
+
+function changeImage2() {
+  var image = document.getElementById("img2");
+  if (image.src.match("img2")) {
+    image.src = "./Assets/img1.png";
+  } else {
+    image.src = "./Assets/img2.png";
+  }
+}
+
+function changeImage4() {
+  var image = document.getElementById("img4");
+  if (image.src.match("img4")) {
+    image.src = "./Assets/img3.png";
+  } else {
+    image.src = "./Assets/img4.png";
+  }
+}
+
+function changeImage6() {
+  var image = document.getElementById("img6");
+  if (image.src.match("img6")) {
+    image.src = "./Assets/img5.png";
+  } else {
+    image.src = "./Assets/img6.png";
+  }
+}
