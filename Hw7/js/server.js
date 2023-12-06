3ed8b67e9aaa7f9fba68b356b49ca76483e9f944
Christ <Christ42@vtb.link>
2023-12-06 00:42:25 -0500

    hw7

--- Hw7/js/server.js
+++ Hw7/js/server.js
@@ -1,3 +1,4 @@
+document.querySelector("#weatherCard").style.display = "block"; // 显示卡片
 const clearButton = document.querySelector("#clear");
 const cityInput = document.querySelector("#city");
 const stateInput = document.querySelector("#state");
@@ -15,9 +16,8 @@ const theButton = document
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},us&appid=eca5940af206314b7aa14293b7ddef18`;
     const response = await fetch(url);
     const data = await response.json();
-    theCity = data;
-    console.log(data);
     document.querySelector("#cityName").innerHTML = data.name;
+    document.querySelector("#stateName").innerHTML = data.name;
     document.querySelector("#cityTemp").innerHTML = data.main.temp;
     document.querySelector("#cityHumidity").innerHTML = data.main.humidity;
     document.querySelector("#cityWindSpeed").innerHTML = data.wind.speed;
