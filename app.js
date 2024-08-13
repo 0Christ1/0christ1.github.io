dd36a7bfa5dc40c239c473a339e492e016597248
Christ <hc3539@nyu.edu>
2024-08-13 02:54:45 -0400

    v1.1

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ setInterval(() => {
 
 // text animation
 const timeline = new TimelineMax();
-// const text1 = (text.innerText = 'My name is Christ not Jesus :)');
 timeline
   .fromTo(text1, 3, { opacity: 1 }, { opacity: 0 })
   .fromTo(text2, 3, { opacity: 0 }, { opacity: 1 })
@@ -51,3 +50,39 @@ let textScene = new ScrollMagic.Scene({
 })
   .setTween(timeline)
   .addTo(controller);
+
+// text1 animation
+const text1Cycle = new TimelineMax({ repeat: -1, paused: true });
+text1Cycle
+  .to(text1, 1.5, { opacity: 1 })
+  .to(text1, 0.5, { opacity: 0 })
+  .add(() => {
+    text1.textContent = '你好';
+  })
+  .to(text1, 1.5, { opacity: 1 })
+  .to(text1, 0.5, { opacity: 0 })
+  .add(() => {
+    text1.textContent = 'Bonjour';
+  })
+  .to(text1, 1.5, { opacity: 1 })
+  .to(text1, 0.5, { opacity: 0 })
+  .add(() => {
+    text1.textContent = 'Hola';
+  })
+  .to(text1, 1.5, { opacity: 1 })
+  .to(text1, 0.5, { opacity: 0 })
+  .add(() => {
+    text1.textContent = 'こんにちは';
+  })
+  .to(text1, 1.5, { opacity: 1 })
+  .to(text1, 0.5, { opacity: 0 });
+
+// Listen for scroll events to stop the animation when scrolling begins
+videoScene.on('update', function (e) {
+  if (e.scrollPos === 0) {
+    text1Cycle.play();
+  } else if (e.scrollPos > 0) {
+    text1Cycle.pause();
+    text1Cycle.seek(0);
+  }
+});
